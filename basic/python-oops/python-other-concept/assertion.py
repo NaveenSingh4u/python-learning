9d8613c99b5f31d4930d08c875a8d8729496b8e4
naveensingh4u <singh.naveen0004@gmail.com>
2020-02-16 15:33:53 +0530

    Excercise on assertion

---
+++ basic/python-oops/python-other-concept/assertion.py
@@ -0,0 +1,12 @@
+x = 10
+
+# The following line will throw assertion error
+# if number is not greater then 10
+# and flow will not move further
+assert x > 10
+
+# Assertion with message
+# if number is not greater then 20
+assert x > 20, 'Here number should be greater than 20'
+
+print(x)
